74a56c007b47c9bab51ea5a68612771f68a45889
supreme-ai-gif <obuegenesis9@gmail.com>
2026-01-02 15:34:47 +0100

    Create admin.js

---
+++ js/admin.js
@@ -0,0 +1,19 @@
+import { supabase } from "./supabase.js";
+
+createTab.onclick = async () => {
+  const name = prompt("Tab name");
+  await supabase.from("tabs").insert({ name });
+};
+
+postProduct.onclick = async () => {
+  const name = prompt("Product name");
+  const price = prompt("Price");
+  const link = prompt("Buy link");
+
+  await supabase.from("products").insert({
+    name,
+    price,
+    buy_link: link,
+    views: 0
+  });
+};
