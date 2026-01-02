07d06cc17f12acfec816f43084d806c2cea32788
supreme-ai-gif <obuegenesis9@gmail.com>
2026-01-02 15:18:42 +0100

    Create landing.js

---
+++ js/landing.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+  document.querySelector('.logo').classList.add('active');
+}, 600);
+
+setTimeout(() => {
+  document.querySelector('.actions').classList.add('show');
+}, 1500);
