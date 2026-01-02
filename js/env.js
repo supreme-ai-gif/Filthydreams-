550baa5fc608395084b47499d92eeb3c081ba525
supreme-ai-gif <obuegenesis9@gmail.com>
2026-01-02 16:14:21 +0100

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
 window.__ENV__ = {
-  SUPABASE_URL: "https://YOUR_PROJECT_ID.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_PUBLIC_ANON_KEY"
+  SUPABASE_URL: "https://mjyrmzpjlwuzthqrxbcs.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_iKOVvqoKvnaSzqanF2SElA_Qz_G-GaT"
 };
