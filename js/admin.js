0568d2f7796afaad18af74b19e67ab84cbc696c9
supreme-ai-gif <obuegenesis9@gmail.com>
2026-01-03 00:13:45 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,7 @@
 import { supabase } from "./supabase.js";
-
+if (localStorage.getItem("isAdmin") !== "true") {
+  window.location.href = "../login.html";
+}
 createTab.onclick = async () => {
   const name = prompt("Tab name");
   await supabase.from("tabs").insert({ name });
