59869b0b96a4fcd627d87ad02a8b4eaf82e1f4f6
supreme-ai-gif <obuegenesis9@gmail.com>
2026-01-03 22:50:58 +0100

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
-const { SUPABASE_URL, SUPABASE_ANON_KEY } = window.__ENV__;
-
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
