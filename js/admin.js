d165d122c62dc1433d6c4b8858dd7f8d6bba9116
supreme-ai-gif <obuegenesis9@gmail.com>
2026-01-03 22:42:03 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,21 +1,3 @@
-import { supabase } from "./supabase.js";
 if (localStorage.getItem("isAdmin") !== "true") {
   window.location.href = "../login.html";
 }
-createTab.onclick = async () => {
-  const name = prompt("Tab name");
-  await supabase.from("tabs").insert({ name });
-};
-
-postProduct.onclick = async () => {
-  const name = prompt("Product name");
-  const price = prompt("Price");
-  const link = prompt("Buy link");
-
-  await supabase.from("products").insert({
-    name,
-    price,
-    buy_link: link,
-    views: 0
-  });
-};
