4b73612525baab80bd693a55be74db7f43c89c6f
supreme-ai-gif <obuegenesis9@gmail.com>
2026-01-03 22:50:09 +0100

    Delete js/landing.js

--- js/landing.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-  document.querySelector('.logo').classList.add('active');
-}, 600);
-
-setTimeout(() => {
-  document.querySelector('.actions').classList.add('show');
-}, 1500);
